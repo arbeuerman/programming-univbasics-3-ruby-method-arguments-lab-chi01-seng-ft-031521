d5c510b88d83762eb541cbdbccad508717bcffc7
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-11 20:05:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,9 +13,9 @@ def introduction_with_language name, language
   "Hi my name is #{name} and I am learning to program in #{language}."
 end 
 
-puts introduction_with_language("Ronnie", "Ruby")
+puts introduction_with_language("Ronnie", "Python")
 
-def introduction_with_language_optional(name, language="Python") 
+def introduction_with_language_optional(name, language="Ruby") 
   "Hi my name is #{name} and I am learning to program in #{language}."
 end 
 
