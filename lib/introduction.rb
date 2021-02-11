1b2e197b44b78204298c9e0c43804c88bf2309ea
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-11 20:03:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,8 +9,8 @@ end
 
 puts introduction("Alexandra")
 
-def introduction_with_language name language 
-  "Hi my name is #{name} and I am learning to program in #{language}"
+def introduction_with_language name, language 
+  "Hi my name is #{name} and I am learning to program in #{language}."
 end 
 
 puts introduction_with_language("Alexandra", "Ruby")
